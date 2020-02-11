6de219e6d661b85a09207ee02e87795a97a24340
nethjai <42929833+nethjai@users.noreply.github.com>
2020-02-11 11:33:29 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+hostname=$(hostname -i)
+echo "the IP-address of this host is $hostname"
+echo "root:root" | chpasswd
