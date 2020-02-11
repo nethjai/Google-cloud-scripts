90ea51721a310d88daa23b7723945df7c21055a9
nethjai <42929833+nethjai@users.noreply.github.com>
2020-02-11 10:34:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,3 +6,4 @@ echo "mnethaji999 ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+service sshd restart
