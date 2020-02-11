5c4f0569f2c4cb28002180a74e8c44a5095788a5
nethjai <42929833+nethjai@users.noreply.github.com>
2020-02-11 09:46:04 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+sudo su
+echo "mn" | passwd --stdin mnethaji999
+echo "mnethaji999 ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+#sed -i '27i mnethaji999 ALL=(ALL) NOPASSWD:ALL 27' /etc/sudoers
+#sed '27 a mnethaji999 ALL=(ALL) NOPASSWD:ALL ' /etc/sudoers
+
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
