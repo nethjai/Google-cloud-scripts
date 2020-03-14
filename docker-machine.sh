135fd6d8da73b4db7deac2af7a7e39f413bcc525
nethjai <42929833+nethjai@users.noreply.github.com>
2020-03-14 13:29:34 +0530

    Update docker-machine.sh

--- docker-machine.sh
+++ docker-machine.sh
@@ -2,3 +2,4 @@
  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
  chmod +x /usr/local/bin/docker-machine
+ docker-machine --version
